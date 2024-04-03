9fe6c471bad0c41e0f51e889650861a3ab19c73a
AbdouBenhamideche <abderrahmanebenhamideche@gmail.com>
2024-04-03 15:36:14 -0400

    V4

--- client_udp.py
+++ client_udp.py
@@ -3,7 +3,7 @@ import os
 
 SERVER_ADDRESS = "localhost"
 SERVER_PORT = 21222
-TAILLE_MAX_SEGMENT = 1024
+TAILLE_MAX_SEGMENT = 2048
 
 
 
@@ -18,24 +18,46 @@ if dn == b"CONNECTION RECUE": #si la connexion est bien établie
 
 
     fichierEnvoie = input("veuillez entrer le nom du fichier souhaité") #entrer le nom du fichire souhaitéé, il sera rechercher dans le dossier actuel si il existe il sera envoyé sinon on revoie message d'erreur 
+    fichierEnvoie = fichierEnvoie.encode()
+    client_socket.sendto(fichierEnvoie,(adr)) #envoi du nom du fichier a envoyer au serveur
 
 
 
     donne_recu = b""
     numeroSegment = 0
-    while True:
-        data, addr = client_socket.recvfrom(TAILLE_MAX_SEGMENT) 
-        numeroSegment += 1
-        client_socket.sendto(b"segmentRecu",addr)
-        print("segment " + str(numeroSegment) + " recu avec succées")
-        if data == b"TERMINE":
-            break
 
-        donne_recu += data
 
-    with open("FichierRecu.bin" ,"wb") as file :
-        file.write(donne_recu)
-    print("fichier recu avec succée")
+    
+    V , addr = client_socket.recvfrom(TAILLE_MAX_SEGMENT)
+    print (V)
+    V = (V.decode())
+
+    if V == "1": #si le nom correspend 
+
+
+        print("fichier trouvé")
+
+        while True:
+            data, addr = client_socket.recvfrom(TAILLE_MAX_SEGMENT * 5) 
+            numeroSegment += 1
+            client_socket.sendto(b"segmentRecu",addr)
+            print("bloc de segment " + str(numeroSegment) + " recu avec succées")
+            if data == b"TERMINE":
+                break
+
+            donne_recu += data
+
+        with open("FichierRecu.bin" ,"wb") as file :
+            file.write(donne_recu)
+        print("fichier recu avec succée")
+    else:
+        print("le fichier n'existe pas.")    #si le nom ne correspond pas
+
+
+
+
+
+
 else:     #si y avais pas d accusé de reception de connexion recu
     print("Erreur de connection")
 
--- server_udp.py
+++ server_udp.py
@@ -3,20 +3,56 @@ import os
 
 SERVER_ADDRESS = "localhost"
 PORT = 21222
-TAILLE_MAX_SEGMENT = 1024
+TAILLE_MAX_SEGMENT = 2048
+N = 5
+
+
+def rechercher_fichier(nom_fichier):
+    # Récupérer le chemin absolu du répertoire actuel
+
+    chemin_repertoire = os.getcwd()
+    
+    # Vérifier si le fichier existe dans le répertoire actuel
+    if nom_fichier in os.listdir(chemin_repertoire):
+        print (f"Le fichier '{nom_fichier}' a été trouvé dans le répertoire actuel.")
+        return 1
+    else:
+        print (f"Le fichier '{nom_fichier}' n'a pas été trouvé dans le répertoire actuel.")
+        return 0
+
 
 def  SendFile(sock, addr, nomFichier):
-    with open(nomFichier, 'rb') as file:
-        while True:
-            segment =  file.read(TAILLE_MAX_SEGMENT)
+    try:
+        with open(nomFichier, 'rb') as file:
+            blocSegment = b""
+            n = 0
+            m = 0
+            while True:
+                segment = file.read(TAILLE_MAX_SEGMENT)
+                if not segment:
+                    break
+                
+                blocSegment += segment
+                n += 1
+                m += 1
+                print(m)
+                if n == N:
+                    sock.sendto(blocSegment, addr)
+                    msg, adr = sock.recvfrom(TAILLE_MAX_SEGMENT)  # Acknowledge receipt for each block of segments
+                    blocSegment = b""
+                    n = 0
 
-            if not segment:
-                break
+            if blocSegment:
+                sock.sendto(blocSegment, addr)  # Send the remaining block of segments
+                msg, adr = sock.recvfrom(TAILLE_MAX_SEGMENT)  # Acknowledge receipt for the last block of segments
 
-            sock.sendto(segment, addr)
-            msg, adr = sock.recvfrom(TAILLE_MAX_SEGMENT) #accuse de recepption pour chaque segment !!!!!!
-        sock.sendto(b"TERMINE", addr)
-        print("fichier envoyé avec succée")
+            sock.sendto(b"TERMINE", addr)
+            print("Fichier envoyé avec succès")
+    except OSError as e:
+        if e.errno == 10040:
+            print("Erreur: La taille du message dépasse la limite du tampon de socket. Essayez de réduire la taille du message ou d'augmenter la taille du tampon.")
+        else:
+            print("Erreur:", e)
 
 
 
@@ -30,8 +66,15 @@ while True:
     if data == b"DEMANDE DE CONNEXION":
         print("connexion établie")
         server_socket.sendto(b"CONNECTION RECUE",client_address)
-        SendFile(server_socket, client_address,  'testfile.txt')
-        break
+        nomFile, client_address = server_socket.recvfrom(TAILLE_MAX_SEGMENT)
+        v = rechercher_fichier(nomFile.decode())
+        server_socket.sendto((str(v).encode()), client_address)
+        if v:
+            SendFile(server_socket, client_address,  nomFile.decode())
+            break
+        else:
+            break
+            
     else:
         print("Connexion non établie")
         break
